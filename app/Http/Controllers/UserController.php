055fed22d531e7bb333658ba85be879c0520abb6
asatriana <asatriana@gmail.com>
2021-12-24 00:12:42 +0700

    add positions as foreign key

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
+use App\Models\Position;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -26,7 +27,8 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('users.create');
+        $positions = Position::all();
+        return view('users.create', compact('positions'));
     }
 
     /**
@@ -42,6 +44,7 @@ class UserController extends Controller
             'name' => 'required',
             'email' => 'required',
             'password' => 'required',
+            
         ]);
 
         //fungsi eloquent untuk menambah data
