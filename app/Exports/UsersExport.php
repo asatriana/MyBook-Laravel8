d18b4d9e8059f1489b83878669744972b5619aba
asatriana <asatriana@gmail.com>
2021-12-29 14:32:00 +0700

    add library dompdf

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -6,18 +6,18 @@ use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class UsersExport implements FromCollection//, WithHeadings
+class UsersExport implements FromCollection, WithHeadings
 {
-    // public function headings():array {
-        // return [
-            // 'id',
-            // 'name',
-            // 'email',
-            // 'created_at',
-            // 'updated_at',
-            // 'position_id',
-        // ];
-    // }   
+    public function headings():array {
+        return [
+            'id',
+            'name',
+            'email',
+            'created_at',
+            'updated_at',
+            'position_id',
+        ];
+    }   
     /**
     * @return \Illuminate\Support\Collection
     */
